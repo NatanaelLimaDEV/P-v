584661337daa1621fcc1a0a1a079f431b32c3676
NatanaelLimaDEV <natanaellimaofc@gmail.com>
2024-07-16 09:31:55 -0300

    atualizacoes

---
+++ script.js
@@ -0,0 +1,8 @@
+let img1 = document.getElementById('imagem1')
+let img2 = document.getElementById('imagem2')
+let img3 = document.getElementById('imagem3')
+let img4 = document.getElementById('imagem4')
+let img5 = document.getElementById('imagem5')
+
+let album = ['imagens/pegadas.jpg', 'imagens/sol.jpg', 'imagens/coqueiro.jpg', 'imagens/chinelo.jpg']
+
